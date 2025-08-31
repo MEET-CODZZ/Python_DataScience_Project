f5f93e0bdb5f2346c69842a6bae76a1bb56800ea
manmeet_mv <vaidyamanmeet29@gmail.com>
2025-08-31 16:14:55 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,47 @@
+# flask , pandas , scikit-learn , pickle-mixin
+
+from flask import Flask , render_template , request
+import pandas as pd
+import numpy as np
+import json
+import pickle
+
+
+app = Flask(__name__)
+#  C:\Users\MANMEET\OneDrive\Desktop\Coding\Data science using python\Projects
+model = pickle.load(open(r"C:\Users\MANMEET\OneDrive\Desktop\Coding\Data science using python\Projects\LinearRegressionModel.pkl","rb"))
+df = pd.read_csv(r"C:\Users\MANMEET\OneDrive\Desktop\Coding\Data science using python\Projects\Cleaned_Car.csv")
+
+@app.route('/')
+
+def index():
+    companies = sorted(df["company"].unique())
+    car_models = sorted(df["name"].unique())
+    year = sorted(df["year"].unique())
+    fuel_type = df["fuel_type"].unique()
+    companies.insert(0,"Select Company")
+    car_models.insert(0,"Select Model")
+    return render_template("index.html", companies = companies, car_models = car_models, years = year, fuel_types = fuel_type)
+
+
+
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    company = request.form.get('company')
+    car_model = request.form.get("car_model")
+    year = int(request.form.get("year"))
+    fuel_type = request.form.get("fuel_type")
+    kms_driven = int(request.form.get("kilo_driven"))
+
+
+
+    prediction  = model.predict(pd.DataFrame([[car_model, company, year, kms_driven, fuel_type]], columns=['name', 'company', 'year', 'kms_driven', 'fuel_type']))
+
+    
+    return str(np.round(prediction[0],2))
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
